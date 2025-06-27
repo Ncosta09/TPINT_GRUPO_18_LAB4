b5e002f5cd001744e6261ee9df75c2dca80e014a
Francisco Muslera <francisco.muslera@gmail.com>
2025-06-26 22:33:59 -0300

    resolve merge conflict

--- src/main/java/dao/ClienteDao.java
+++ src/main/java/dao/ClienteDao.java
@@ -5,12 +5,7 @@ import dominio.Cliente;
 
 public interface ClienteDao {
 
-<<<<<<< HEAD
 	List<Cliente> obtenerTodos();
 	boolean darDeBaja(int idUsuario);
-	
-=======
 	boolean altaCliente(Cliente cliente);
-	List<Cliente> obtenerTodos();	
->>>>>>> 75bcb2ae5945837531de9bfe2fcd64d2c42584af
 }
