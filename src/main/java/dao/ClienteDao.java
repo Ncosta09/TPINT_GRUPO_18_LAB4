6d7058d53057138efff1b109177ceba72030e3ca
Francisco Muslera <francisco.muslera@gmail.com>
2025-06-26 23:50:19 -0300

    fix common component

--- src/main/java/dao/ClienteDao.java
+++ src/main/java/dao/ClienteDao.java
@@ -7,12 +7,8 @@ public interface ClienteDao {
 
 	List<Cliente> obtenerTodos();
 	boolean darDeBaja(int idUsuario);
-<<<<<<< HEAD
 	boolean altaCliente(Cliente cliente);	
 	Cliente obtenerPorId(int idCliente);
 	boolean modificarCliente(Cliente c);
 
-=======
-	boolean altaCliente(Cliente cliente);
->>>>>>> e65367dc35d83015c6bc750c0ecad6c38b47ac86
 }
